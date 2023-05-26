f456f69cea0c8155ea1658f96c8ea7ebfb3d98ff
Akshaya-SM <126845727+Akshaya-SM@users.noreply.github.com>
2023-05-26 17:23:46 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VHARCHAR(15) NOT NULL,
+	dept_no VARCHAR(15) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
